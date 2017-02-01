2a7b76bac6415db5b687edcd4856d2f854b82862
Shimei Wong <wshimei@gmail.com>
2017-02-01 14:59:56 +0800

    included gameLost and gameOver

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-   // var level = 1
-   // var timer = 0
   var moles = document.querySelectorAll('.mole')
   var ouch = document.querySelectorAll('.ouch')
   var startBtn = document.querySelector('button')
@@ -10,6 +8,31 @@ $(document).ready(function () {
   var mistake = 0
   var randomMole
   var whacked
+
+  startBtn.addEventListener('click', function () {
+    var randomNum = Math.floor(Math.random() * 7)
+    var appearInterval = 1500
+    var disappearInterval = 2500
+
+    if (score === 10) {
+      appearInterval = 750
+      disappearInterval = 1400
+      level.innerHTML = 'Level: 2'
+    } else if (score === 25) {
+      appearInterval = 375
+      disappearInterval = 500
+      level.innerHTML = 'Level: 3'
+    }
+
+    setInterval(function () {
+      moleAppear(randomNum)
+    }, appearInterval)
+
+    setInterval(function () {
+      moleDisappear(randomNum)
+      randomNum = Math.floor(Math.random() * 7)
+    }, disappearInterval)
+  })
 
   function moleAppear (i) {
     console.log('moleAppear setTimeout')
@@ -26,87 +49,40 @@ $(document).ready(function () {
   }
 
   $(document).keypress(function (e) {
-    // var mole = document.querySelector('.mole')
     if (e.key === randomMole.parentNode.id) {
       randomMole.classList.add('hidden')
       whacked.classList.remove('hidden')
       score += 1
       scoreboard.textContent = 'Score: ' + score
+      gameOver()
 
       setTimeout(function () {
         whacked.classList.add('hidden')
-      }, 500)
+      }, 300)
     } else if (e.key !== randomMole.parentNode.id) {
       mistake += 1
+      gameLost()
     }
   })
 
-  // function levelTwo () {
-  //   if (score === 10) {
-  //     level.textContent = 'Level: 2'
-  //     setInterval(moleAppear, 2000)
-  //   }
-  //   levelThree()
-  // }
-  //
-  // function levelThree () {
-  //   if (score === 30) {
-  //     level.textContent = 'Level: 3'
-  //     setInterval(moleAppear, 1000)
-  //   }
-  //   gameOver()
-  // }
-
-  startBtn.addEventListener('click', function () {
-    var randomNum = Math.floor(Math.random() * 7)
-    
-    // TODO
-    // change the appearInterval upon score change
-
-    var appearInterval = 1500
-
-    // if (score === 10) {
-    //   appearInterval = 750
-    //   level.textContent = 'Level: 2'
-    // } if (score === 25) {
-    //   appearInterval = 375
-    //   level.textContent = 'Level: 3'
-    // }
-
-    var disappearInterval = 2500
-
-    // if (score === 10) {
-    //   appearInterval = 1400
-    // } else if (score === 25) {
-    //   appearInterval = 500
-    // }
-
-    setInterval(function () {
-      moleAppear(randomNum)
-    }, appearInterval)
-
-    setInterval(function () {
-      moleDisappear(randomNum)
-      randomNum = Math.floor(Math.random() * 7)
-    }, disappearInterval)
-
-  })
-
   function gameLost () {
-    if (mistake === 2) {
+    if (mistake === 3) {
       alert('Too bad, try again?')
       score = 0
-      level.textContent = 'Level: 1'
-      window.location.reload(false)
+      reset()
     }
   }
 
   function gameOver () {
     if (score === 50) {
-      alert('WAY TO GO! PLAY AGAIN?')
-      score = 0
-      level.textContent = 'Level: 1'
-      window.location.reload(false)
+      alert('WAY TO GO! PLAY AGAIN!')
+      reset()
     }
+  }
+
+  function reset () {
+    score = 0
+    level.textContent = 'Level: 1'
+    window.location.reload(false)
   }
 })
